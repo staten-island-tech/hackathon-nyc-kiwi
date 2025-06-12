dcc97b8f9a3f36c3aebc446d58f13199dbe8331d
AIfred-Jiang <alfredj14@nycstudents.net>
2025-06-12 10:10:51 -0400

    commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import folium
+import googlemaps
+import time
+
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('map.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
